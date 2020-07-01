938c6ba4951f003dc9b6bbcfe8f9065585a60530
Raul Sposito <raulsposito@gmail.com>
2020-07-01 20:09:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears VALUES ("Mr. Chocolate", 33, "M", "Brown", "Happy", 1);
+INSERT INTO bears VALUES ("Rowdy", 15, "M", "Black", "Curious", 1);
+INSERT INTO bears VALUES ("Tabitha", 15, "M", "Black", "Curious", 1);
+INSERT INTO bears VALUES ("Sergeant Brown", 15, "M", "Black", "Curious", 1);
+INSERT INTO bears VALUES ("Melissa", 15, "M", "Black", "Curious", 1);
+INSERT INTO bears VALUES ("Grinch", 15, "M", "Black", "Curious", 1);
+INSERT INTO bears VALUES ("Wendy", 15, "M", "Black", "Curious", 1);
+INSERT INTO bears VALUES ("", 15, "M", "Black", "Curious", 1);
