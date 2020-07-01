cb78a3ce9dfb814badb17e7f1e626aaab9b37ffe
Raul Sposito <raulsposito@gmail.com>
2020-07-01 20:15:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES ("Mr. Chocolate", 33, "M", "Brown", "Happy", 1);
-INSERT INTO bears VALUES ("Rowdy", 15, "M", "Black", "Curious", 1);
-INSERT INTO bears VALUES ("Tabitha", 15, "F", "Black", "Furious", 1);
-INSERT INTO bears VALUES ("Sergeant Brown", 15, "M", "Black", "Nerdy", 1);
-INSERT INTO bears VALUES ("Melissa", 15, "F", "Black", "Restless", 1);
-INSERT INTO bears VALUES ("Grinch", 15, "M", "Pale", "Agressive", 1);
-INSERT INTO bears VALUES ("Wendy", 15, "F", "Black", "Curious", 1);
-INSERT INTO bears VALUES ("", 15, "M", "Pale Brown", "Agressive", 1);
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 33, "M", "Brown", "Happy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 15, "M", "Black", "Curious", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 15, "F", "Black", "Furious", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 15, "M", "Black", "Nerdy", 1);
+INSERT INTO bears VALUES (5, "Melissa", 15, "F", "Black", "Restless", 1);
+INSERT INTO bears VALUES (6, "Grinch", 15, "M", "Pale", "Agressive", 1);
+INSERT INTO bears VALUES (7, "Wendy", 15, "F", "Black", "Curious", 1);
+INSERT INTO bears VALUES (8, "", 15, "M", "Pale Brown", "Agressive", 1);
