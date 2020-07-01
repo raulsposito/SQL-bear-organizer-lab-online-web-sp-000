5455e073f8d66e2471e3711f9ce95f0e7c63cd67
Raul Sposito <raulsposito@gmail.com>
2020-07-01 20:17:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears VALUES (4, "Sergeant Brown", 15, "M", "Black", "Nerdy", 1);
 INSERT INTO bears VALUES (5, "Melissa", 15, "F", "Black", "Restless", 1);
 INSERT INTO bears VALUES (6, "Grinch", 15, "M", "Pale", "Agressive", 1);
 INSERT INTO bears VALUES (7, "Wendy", 15, "F", "Black", "Curious", 1);
-INSERT INTO bears VALUES (8, "", 15, "M", "Pale Brown", "Agressive", 1);
+INSERT INTO bears VALUES (8, NULL, 15, "M", "Pale Brown", "Agressive", 1);
